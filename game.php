8c03dfb88ef609ab251ab2d5d8c5dd105fa1edc2
Lemar Andar <lemarandar@outlook.com>
2022-11-09 23:06:33 -0500

    text affiché

--- game.php
+++ game.php
@@ -8,7 +8,53 @@
 ?>
 <script src="./js/game.js"></script>
 <div onload="state()">
-    <div id="state"></div>
+    <div id="test"></div>
+    <br>    
+    <div id="turntime"></div>
+    <div id="yourturn"></div>
+    <div id="heropowerused"></div>
+    <div id="hp"></div>
+    <div id="maxhp"></div>
+    <div id="mp"></div>
+    <div id="maxmp"></div>
+    
+    <br>
+    <div id="hand">
+        <div id="handcard1"></div>
+    </div>
+
+    <br>
+    <div id="board">
+        <div id="boardcard1"></div>
+    </div>
+
+    <br>
+    <div id="welcometext"></div>
+    <div id="heroclass"></div>
+    <div id="talent"></div>
+    <div id="remainingcardcount"></div>
+
+    <br>
+    <div>OPPONENT</div>
+    <div id="opusername"></div>
+    <div id="opclass"></div>
+    <div id="ophp"></div>
+    <div id="opmaxhp"></div>
+    <div id="opmp"></div>
+    <div id="opmaxmp"></div>
+    <div id="opboard">
+        <div id="opcard1"></div>
+    </div>
+    <div id="optext"></div>
+    <div id="opcardcount"></div>
+    <div id="ophandsize"></div>
+    <div id="optrophycount"></div>
+    <div id="opwincount"></div>
+    <div id="oplosscount"></div>
+    <div id="optalent"></div>
+
+    <br>
+    <div id="lastestactions"></div>
 </div>
 
 <?php
--- js/game.js
+++ js/game.js
@@ -23,7 +23,36 @@ const state = () => {
             window.location.href = 'index.php';
         }
         else{
-            document.querySelector("#state").innerHTML = data["remainingTurnTime"];
+            document.querySelector("#test").innerHTML = data.opponent.heroClass;
+            
+            document.querySelector("#turntime").innerHTML = "Turn Time: " + data["remainingTurnTime"];
+            document.querySelector("#yourturn").innerHTML = "Your turn: " + data["yourTurn"];
+            document.querySelector("#heropowerused").innerHTML = "Power Used: " + data["heroPowerAlreadyUsed"];
+            document.querySelector("#hp").innerHTML = "Health: " + data["hp"];
+            document.querySelector("#maxhp").innerHTML = "Max HP: " + data["maxHp"];
+            document.querySelector("#mp").innerHTML = "Magic: " + data["mp"];
+            document.querySelector("#maxmp").innerHTML = "Max Magic: " + data["maxMp"];
+
+            document.querySelector("#hand").innerHTML = "Cards in Hand: " + JSON.stringify(data["hand"]);
+
+            document.querySelector("#board").innerHTML = "Board: " + JSON.stringify(data["board"]);
+
+            document.querySelector("#welcometext").innerHTML = "Welcome Text: " + data["welcomeText"];
+            document.querySelector("#heroclass").innerHTML = "Hero Class: " + data["heroClass"];
+            document.querySelector("#remainingcardcount").innerHTML = "Remaining Cards: " + data["remainingCardsCount"];
+
+            document.querySelector("#opusername").innerHTML = "user: " + data.opponent.username;
+            document.querySelector("#opclass").innerHTML = "heroclass: " + data.opponent.heroClass;
+            document.querySelector("#ophp").innerHTML = "hp" + data.opponent.hp;
+            document.querySelector("#opboard").innerHTML = "board" + data.opponent.board;
+            document.querySelector("#optext").innerHTML = "welcometext: " + data.opponent.welcomeText;
+            document.querySelector("#opcardcount").innerHTML = "card count: " + data.opponent.remainingCardsCount;
+            document.querySelector("#optrophycount").innerHTML = "trophy count: " + data.opponent.trophyCount;
+            document.querySelector("#opwincount").innerHTML = "win count: " + data.opponent.winCount;
+            document.querySelector("#oplosscount").innerHTML = "loss count: " + data.opponent.lossCount;
+            document.querySelector("#optalent").innerHTML = "talent: " + data.opponent.talent;
+
+            document.querySelector("#lastestactions").innerHTML = "actions: " + JSON.stringify(data["latestActions"]);
         }
         setTimeout(state, 1000); // Attendre 1 seconde avant de relancer l’appel
     });
