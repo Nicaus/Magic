859313827e6d4a68ac14a5620d2f782880fba039
Lemar Andar <lemarandar@pm.me>
2022-11-14 14:20:47 -0500

    game/action works ayyyy LFGOOOOOO

--- js/game.js
+++ js/game.js
@@ -1,3 +1,4 @@
+let len = 0;
 const state = () => {
     fetch("ajax-state.php", { // Il faut créer cette page et son contrôleur appelle
         method : "POST" // l’API (games/state)
@@ -27,7 +28,7 @@ const state = () => {
             document.querySelector("#mp").innerHTML = "Magic: " + data["mp"];
             document.querySelector("#maxmp").innerHTML = "Max Magic: " + data["maxMp"];
 
-            document.querySelector("#hand").innerHTML = "Cards in Hand: " + JSON.stringify(data["hand"], undefined, 2);
+            // document.querySelector("#hand").innerHTML = "Cards in Hand: " + JSON.stringify(data["hand"], undefined, 2);
             // console.log(JSON.stringify(data["hand"], null, 2));
 
             document.querySelector("#board").innerHTML = "yourboard: " + JSON.stringify(data["board"]);
@@ -54,64 +55,64 @@ const state = () => {
 
 
             const endturn = document.querySelector("#endturn");
-            endturn.addEventListener('click', (e) => {
-                // data.latestActions
-                data["type"] = "END_TURN";
-                console.log(data.type);
-
+            endturn.addEventListener('click', () => {
+                gameaction("END_TURN");
             });
 
             const surrender = document.querySelector("#surrender");
-            surrender.addEventListener('click', (e) => {
-                data["type"] = "SURRENDER";
-                console.log(data.type);
+            surrender.addEventListener('click', () => {
+                gameaction("SURRENDER");
             });
 
             // https://stackoverflow.com/questions/69001483/how-to-generate-multiple-card-for-each-object-in-an-array
             let hand = document.querySelector("#hand");
 
             const cardsjs = data["hand"];
-            cardsjs.forEach(cardjs => {
-                // cardjs.addEventListener("load", (e) =>{
-                //     const index = Array.from(cardsjs).indexOf(e.target);
-                //     const indes = Array.from(cardsjs).toString;
-                //     console.log(index);
-                //     console.log(indes);
-                // })
-                // console.log(cardjs);
-                const card = `<div class="card">
-                                <img src="img/i01_cat.jpg" alt="card img">
-                                <div class="desc">${JSON.stringify(cardjs)}</div>
-                            </div>`
+            let length = cardsjs.length;
+            if (len == length){
+                cardsjs.forEach(cardjs => {
+                    // cardjs.addEventListener("load", (e) =>{
+                    //     const index = Array.from(cardsjs).indexOf(e.target);
+                    //     const indes = Array.from(cardsjs).toString;
+                    //     console.log(index);
+                    //     console.log(indes);
+                    // })
+                    // console.log(cardjs);
+                    const card = `<div class="card">
+                                    <img src="img/i01_cat.jpg" alt="card img">
+                                    <div class="desc">${JSON.stringify(cardjs)}</div>
+                                </div>`
 
-                const element = document.createElement('div');
-                element.innerHTML = card;
-                hand.appendChild(element.firstChild);
-            });
+                    const element = document.createElement('div');
+                    element.innerHTML = card;
+                    hand.appendChild(element.firstChild);
+                    len = length
+                });
+            }
 
             // bhay de data 
-            // $data["type"] = "END_TURN";
-            // $data["type"] = "SURRENDER";
-            // $data["type"] = "HERO_POWER";
-            // $data["type"] = "PLAY";
+            // $data["type"] = "END_TURN"; "SURRENDER"; "HERO_POWER"; "PLAY"; "ATTACK";
             // $data["uid"] = $_SESSION["uid"];
-            // $data["type"] = "ATTACK";
             // $data["uidattack"] = $_SESSION["uidattack"];
-            data.execute;
         }
         setTimeout(state, 1000); // Attendre 1 seconde avant de relancer l’appel
     });
 }
 
-// const cardsjs = document.querySelectorAll("card");
-// cardsjs.forEach(cardjs => {
-//     cardjs.addEventListener('drag', (e) =>{
-//         const index = Array.from(cardsjs).indexOf(e.target);
-//         const indes = Array.from(cardsjs).toString;
-//         console.log(index)
-//         console.log(indes)
-//     })
-// });
+const gameaction = (e) => {
+    let data = new FormData();
+
+    if (e == "END_TURN")
+        data.append("type", "END_TURN");
+    else if (e == "SURRENDER")
+        data.append("type", "SURRENDER");
+
+    fetch("ajax-action.php",{
+        method : "post",
+        body : data
+    })
+    .then(response => response.json())
+};
 
 window.addEventListener("load", () => {
     setTimeout(state, 1000); // Appel initial (attendre 1 seconde)
